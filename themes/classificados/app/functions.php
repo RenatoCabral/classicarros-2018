f35fb1b7b2cfeed26279227391b7b7e4be69a247
RenatoCabral <carvalho.renato6@gmail.com>
2018-02-07 00:34:25 -0200

    add function to remove span contact form 7

--- themes/classificados/app/functions.php
+++ themes/classificados/app/functions.php
@@ -460,3 +460,10 @@ if(jQuery('#post_type').val()==='veiculo'){
 function render_category_dropdown_filter( $post_type, $tax, $title ) {
 	require 'partials/public/item-form-filter-category.php';
 }
+
+
+add_filter('wpcf7_form_elements', function($content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+
+    return $content;
+});
